3706026903f1a2a4f0a82e0d812f259d1942ea7d
lukeoliveira1 <lucasoliveirars22@gmail.com>
2023-07-06 16:48:44 -0300

    login functions to class based views

--- users/urls.py
+++ users/urls.py
@@ -4,9 +4,9 @@ from .views import *
 app_name = 'users_urls'
 
 urlpatterns = [
-    path('conta/login/', my_login, name="login"),
-    path('conta/logout/', logout_request, name='logout'),
-    path("conta/registrar/", register, name="registrar"),
+    path('conta/login/', LoginView.as_view(), name="login"),
+    path('conta/logout/', LogoutView.as_view(), name='logout'),
+    path("conta/registrar/", RegisterView.as_view(), name="registrar"),
     path('clients', ClientListView.as_view(), name='list'),
     path('clients/detail/<int:pk>/', ClientDetailView.as_view(), name='detail'),
     path('clients/create/', ClientCreateView.as_view(), name='create'),
--- users/views.py
+++ users/views.py
@@ -5,63 +5,73 @@ from .models import Client
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm 
-
+from django.views import View
 from django.views.generic import ListView, FormView, UpdateView, DetailView, DeleteView
 
 # Create your views here.
 
 #user
 
-def register(request):
-	if request.method == "POST":
-		form = NewUserForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			login(request, user)
-			messages.success(request, "Registration successful." )
-			return redirect("cars_urls:index")
-		messages.error(request, "Unsuccessful registration. Invalid information.")
-	
-	form = NewUserForm()	
-	return render (request=request, template_name="cadastros/cadastrarUsuario.html", context={"register_form":form})
+class RegisterView(View):
+    def get(self, request):
+        form = NewUserForm()
+        return render(request=request, template_name="registrations/register_form.html", context={"register_form": form})
+
+    def post(self, request):
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect("cars_urls:index")
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+        return render(request=request, template_name="registrations/register_form.html", context={"register_form": form})
 
 
-def my_login(request):
-	if request.method == "POST":
-		form = AuthenticationForm(request, data=request.POST)
-		if form.is_valid():
-			username = form.cleaned_data.get('username')
-			password = form.cleaned_data.get('password')
-			user = authenticate(username=username, password=password)
+class LoginView(View):
+    def get(self, request):
+        form = AuthenticationForm()
+        return render(request=request, template_name="login/login.html", context={"form": form})
 
-			if user is not None:
-				login(request, user) 
-				return redirect("cars_urls:index") 
-			else:
-				messages.error(request,"Invalid username or password.")
-		else:
-			messages.error(request,"Invalid username or password.")
-	form = AuthenticationForm()
-	return render(request=request, template_name="login/login.html", context={"form":form})
+    def post(self, request):
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
 
-def logout_request(request):
-	logout(request)
-	messages.info(request, "Você realizou logout com sucesso!") 
-	return redirect("cars_urls:index")
-
-def register_request(request):
-	if request.method == "POST":
-		form = NewUserForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			login(request, user)
-			messages.success(request, "Registration successful." )
-			return redirect("cars_urls:index")
-		messages.error(request, "Unsuccessful registration. Invalid information.")	
-	form = NewUserForm()
-	return render (request=request, template_name="cadastros/cadastrarUsuario.html", context={"register_form":form})
+            if user is not None:
+                login(request, user)
+                return redirect("cars_urls:index")
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+        return render(request=request, template_name="login/login.html", context={"form": form})
 
 
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        messages.info(request, "Você realizou logout com sucesso!")
+        return redirect("cars_urls:index")
+
+
+class RegisterRequestView(View):
+    def get(self, request):
+        form = NewUserForm()
+        return render(request=request, template_name="registrations/register_form.html", context={"register_form": form})
+
+    def post(self, request):
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect("cars_urls:index")
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+        return render(request=request, template_name="registrations/register_form.html", context={"register_form": form})
+    
 #cliente
 
 class ClientListView(ListView):
@@ -75,7 +85,7 @@ class ClientCreateView(FormView):
     success_url = reverse_lazy('cars_urls:index')
 
     def form_valid(self, form):
-        form.save()  # Salva o objeto Car no banco de dados
+        form.save() 
         return super().form_valid(form)
 
 class ClientUpdateView(UpdateView):
@@ -90,7 +100,73 @@ class ClientDetailView(DetailView):
 
 class ClientDeleteView(DeleteView):
     model = Client
-    slug_field = 'slug'  # campo de slug no modelo Car
-    slug_url_kwarg = 'slug'  # nome do parâmetro na URL
+    slug_field = 'slug'  
+    slug_url_kwarg = 'slug' 
     success_url = reverse_lazy('cars_urls:index')
     template_name = 'clients/client_confirm_delete.html'
+    
+	# class RegisterView(View):
+#     def get(self, request):
+#         form = NewUserForm()
+#         return render(request, "cadastros/register_form.html", {"register_form": form})
+
+#     def post(self, request):
+#         form = NewUserForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)
+#             messages.success(request, "Registration successful.")
+#             return redirect("cars_urls:index")
+#         else:
+#             messages.error(request, "Unsuccessful registration. Invalid information.")
+        
+#         return render(request, "cadastros/register_form.html", {"register_form": form})
+
+# def register(request):
+# 	if request.method == "POST":
+# 		form = NewUserForm(request.POST)
+# 		if form.is_valid():
+# 			user = form.save()
+# 			login(request, user)
+# 			messages.success(request, "Registration successful." )
+# 			return redirect("cars_urls:index")
+# 		messages.error(request, "Unsuccessful registration. Invalid information.")
+	
+# 	form = NewUserForm()	
+# 	return render (request=request, template_name="cadastros/register_form.html", context={"register_form":form})
+
+
+# def my_login(request):
+# 	if request.method == "POST":
+# 		form = AuthenticationForm(request, data=request.POST)
+# 		if form.is_valid():
+# 			username = form.cleaned_data.get('username')
+# 			password = form.cleaned_data.get('password')
+# 			user = authenticate(username=username, password=password)
+
+# 			if user is not None:
+# 				login(request, user) 
+# 				return redirect("cars_urls:index") 
+# 			else:
+# 				messages.error(request,"Invalid username or password.")
+# 		else:
+# 			messages.error(request,"Invalid username or password.")
+# 	form = AuthenticationForm()
+# 	return render(request=request, template_name="login/login.html", context={"form":form})
+
+# def logout_request(request):
+# 	logout(request)
+# 	messages.info(request, "Você realizou logout com sucesso!") 
+# 	return redirect("cars_urls:index")
+
+# def register_request(request):
+# 	if request.method == "POST":
+# 		form = NewUserForm(request.POST)
+# 		if form.is_valid():
+# 			user = form.save()
+# 			login(request, user)
+# 			messages.success(request, "Registration successful." )
+# 			return redirect("cars_urls:index")
+# 		messages.error(request, "Unsuccessful registration. Invalid information.")	
+# 	form = NewUserForm()
+# 	return render (request=request, template_name="cadastros/register_form.html", context={"register_form":form})
